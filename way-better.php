7c46cd70a46a8d07a60adcea4348f1af51c88fcb
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-02-21 15:40:35 +0100

    Add array for clients and then foreach client order the pizza

--- way-better.php
+++ way-better.php
@@ -1,37 +1,34 @@
 <?php
 
-function orderPizza($pizzaType, $client) {
-    $price = calculatePrice($pizzaType);
-    $address = 'unknown';
-    if($client == 'Koen') {
-        $address = 'a yacht in Antwerp';
-    } else if ($client == 'Manuele') {
-        $address = 'somewhere in Belgium';
-    } else if ($client == 'Students') {
-        $address = 'BeCode office';
-    }
-    echo "Creating new order... <br> A {$pizzaType} pizza should be sent to {$client} <br> The address: {$address}. <br>
+$clients = [
+    ['name' => 'Koen', 'pizzaType' => "Calzone", 'address' => 'a yacht in Antwerp'],
+    ['name' => 'Manuele', 'pizzaType' => "Marguerita", 'address' => 'Somewhere in Belgium'],
+    ['name' => 'Students', 'pizzaType' => "Golden", 'address' => 'BeCode office'],
+    ['name' => 'Ruben', 'pizzaType' => "Calzone", 'address' => 'Brugas'],
+];
+
+function orderPizza($clients) {
+    foreach ($clients as $client) {
+        $person = $client["name"];
+        $pizzaType = $client["pizzaType"];
+        $address = $client["address"];
+        $price = calculatePrice($pizzaType);
+        echo "Creating new order... <br> A {$pizzaType} pizza should be sent to {$person} <br> The address: {$address}. <br>
         The bill is € {$price}. <br> Order finished.<br><br>";
+    }
 }
 
+orderPizza($clients);
+
 function calculatePrice($pizzaType) {
-    $price = 'unknown';
     if ($pizzaType == 'Marguerita') {
         $price = 5;
-    } else if ($pizzaType == 'Golden') {
+    }
+    if ($pizzaType == 'Golden') {
         $price = 100;
-    } else if ($pizzaType == 'Calzone') {
+    }
+    if ($pizzaType == 'Calzone') {
         $price = 10;
-    } else {
-        throw new Exception('Computer says no');
     }
     return $price;
 }
-
-function orderPizzaForAll() {
-    orderPizza('Calzone', 'Koen');
-    orderPizza('Marguerita', 'Manuele');
-    orderPizza('Golden', 'Students');
-}
-
-orderPizzaForAll();
