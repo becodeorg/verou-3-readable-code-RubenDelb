ca032a0cb480e36b651e715c40558967cf6ec9e9
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-02-21 14:57:13 +0100

    Delete useless functions and useless variables

--- way-better.php
+++ way-better.php
@@ -1,52 +1,37 @@
 <?php
 
 function orderPizza($pizzaType, $client) {
-    echo 'Creating new order... <br>';
-    $toPrint = 'A ';
-    $toPrint .= $pizzaType;
     $price = calculatePrice($pizzaType);
     $address = 'unknown';
-    if($client == 'koen') {
+    if($client == 'Koen') {
         $address = 'a yacht in Antwerp';
-    } else if ($client == 'manuele') {
+    } else if ($client == 'Manuele') {
         $address = 'somewhere in Belgium';
-    } else if ($client == 'students') {
+    } else if ($client == 'Students') {
         $address = 'BeCode office';
     }
-    $toPrint .=   ' pizza should be sent to ' . $client . ". <br>The address: {$address}.";
-    echo $toPrint . '<br>';
-    echo'The bill is €'.$price.'.<br>';
-    echo "Order finished.<br><br>";
+    echo "Creating new order... <br> A {$pizzaType} pizza should be sent to {$client} <br> The address: {$address}. <br>
+        The bill is € {$price}. <br> Order finished.<br><br>";
 }
 
 function calculatePrice($pizzaType) {
     $price = 'unknown';
-    if ($pizzaType == 'marguerita') {
+    if ($pizzaType == 'Marguerita') {
         $price = 5;
-    } else if ($pizzaType == 'golden') {
+    } else if ($pizzaType == 'Golden') {
         $price = 100;
-    } else if ($pizzaType == 'calzone') {
+    } else if ($pizzaType == 'Calzone') {
         $price = 10;
-    } else if ($pizzaType == 'hawaii') {
+    } else {
         throw new Exception('Computer says no');
     }
     return $price;
 }
 
 function orderPizzaForAll() {
-    $test= 0;
-    orderPizza('calzone', 'koen');
-    orderPizza('marguerita', 'manuele');
-    orderPizza('golden', 'students');
+    orderPizza('Calzone', 'Koen');
+    orderPizza('Marguerita', 'Manuele');
+    orderPizza('Golden', 'Students');
 }
 
-function makeAllHappy($do_it) {
-    if ($do_it) {
-        orderPizzaForAll();
-    } else {
-        // Should not do anything when false
-        return false;
-    }
-}
-
-makeAllHappy(true);
+orderPizzaForAll();
